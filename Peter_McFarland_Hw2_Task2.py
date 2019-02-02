03c8cbd70d49082b254508ce8b9b079c75d5c071
PeterTesla <PeterTesla@gmail.com>
2019-02-02 15:55:50 -0700

    Moved things around into some functions

--- Peter_McFarland_Hw2_Task2.py
+++ Peter_McFarland_Hw2_Task2.py
@@ -16,49 +16,50 @@ your functions.
 Make sure your python module works in dual-mode: by itself or import to other module
 """
 # NOTE: You may need to run: $ pip install matplotlib
+  
+x = []
+y = []
+g = -9.8
 
 # Function to calculate projectile motion
 def px(x,v,t,a):
     return x + v*t + 0.5*a*t**2
 
 # Function to plot data
-def plot_data():
-    pass
+def populate_data(interval):
+    global x
+    global y
 
-# "Main" Function
-def main():
-     
-
-    x0 =  int(input("Please INput Initial X Position: "))
+    x0 =  int(input("Please Input Initial X Position: "))
     vx0 = int(input("Please Input Vx Initial: " ))
 
     y0 = int(input("Please Input Initial Y Position: ")) 
     vy0 = int(input("Please input Vy Initial: ")) 
 
     ax = 0.0
-    ay = -9.8           # define a constant
 
     delt = 0.1
     t = 0.0
 
-   
-    x = []
-    y = []
-
-    interval = 170
 
     for i in range(interval):
        
-        if px(y0,vy0,t,ay) < 0.0:
+        if px(y0,vy0,t,g) < 0.0:
             y.append(0)
-            x.append(px(x0,vx0,(-vy0-m.sqrt(vy0**2 - 2*ay*(y0)))/(ay), ax))
+            x.append(px(x0,vx0,(-vy0 - m.sqrt(vy0**2 - 2*g*(y0)))/(g), ax))
             break
 
         x.append(px(x0,vx0,t,ax))
-        y.append(px(y0,vy0,t,ay))
+        y.append(px(y0,vy0,t,g))
        
         t = t + delt
 
+
+
+
+# "Main" Function
+def main():
+    populate_data(300)
 
     plt.plot(x, y)
     plt.show()
